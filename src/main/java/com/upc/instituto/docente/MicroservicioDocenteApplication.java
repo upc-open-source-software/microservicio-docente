9c3b31babf5724454dbf0d540e920f8811022654
Luis Guisado <lguisadom@gmail.com>
2023-01-30 17:00:02 -0500

    First commit

---
+++ src/main/java/com/upc/instituto/docente/MicroservicioDocenteApplication.java
@@ -0,0 +1,13 @@
+package com.upc.instituto.docente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDocenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/instituto/docente/MicroservicioDocenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.instituto.docente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDocenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
