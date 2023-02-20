5bbfaa6e00a71c7cba7c55a0f0efc158c9393b46
Luis Guisado <lguisadom@gmail.com>
2023-02-20 01:16:11 -0500

    Configuración de Eureka Client

--- src/main/java/com/upc/instituto/docente/MicroservicioDocenteApplication.java
+++ src/main/java/com/upc/instituto/docente/MicroservicioDocenteApplication.java
@@ -2,7 +2,9 @@ package com.upc.instituto.docente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservicioDocenteApplication {
 
